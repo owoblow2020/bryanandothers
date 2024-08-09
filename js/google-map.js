481f4ffeb5d8a5fdcf05f0e87be728419ad3d842
Prezzo <oolulegan@emmanjokufoundation.org>
2024-08-09 16:01:17 +0100

    contact page 3

--- js/google-map.js
+++ js/google-map.js
@@ -1,62 +1,56 @@
-
-var google;
-
+// Removed the unnecessary global variable declaration
 function init() {
+
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
-    
+    var   
+   myLatlng = new google.maps.LatLng(7.419417,   
+   3.926915); // Your desired latitude and longitude
+  
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 7,
-
-        // The latitude and longitude to center the map (always required)
-        center: myLatlng,
-
-        // How you would like to style the map. 
-        scrollwheel: false,
-        styles: [
-            {
-                "featureType": "administrative.country",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "visibility": "simplified"
-                    },
-                    {
-                        "hue": "#ff0000"
-                    }
-                ]
-            }
-        ]
+      zoom: 7, // How zoomed in
+      center: myLatlng, // Center of the map
+      scrollwheel: false, // Disable map zoom on scroll wheel
+      styles: [ // Optional map styles
+        {
+          "featureType": "administrative.country",
+          "elementType": "geometry",
+          "stylers": [
+            { "visibility": "simplified" },
+            { "hue": "#ff0000" }
+          ]
+        }
+      ]
     };
-
-    
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
+  
+    // Get the HTML DOM element that will contain your map   
+  
     var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
+  
+    // Create the Google Map using our element and options   
+  
     var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['New York'];
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
-
-        });
+  
+    var addresses = ['Ibadan, Nigeria.'];
+  
+    // Loop through addresses and create markers
+    for (var i = 0; i < addresses.length; i++) {
+      var address = addresses[i]; // Use a more descriptive variable name
+  
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&sensor=false`)
+        .then(response => response.json())
+        .then(data => {
+          var location = data.results[0].geometry.location;
+          var latlng = new google.maps.LatLng(location.lat, location.lng);
+          new google.maps.Marker({
+            position: latlng,
+            map: map,
+            icon: 'images/loc.png'
+          });
+        })
+        .catch(error => console.error("Error fetching address:", error));
     }
-    
-}
-google.maps.event.addDomListener(window, 'load', init);
+  }
+  
+  // Call the init function when the window loads
+  window.addEventListener('load', init);
